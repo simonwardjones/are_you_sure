1c15584598db2f3ab4af70aa77c25a2b055b8b8e
simonwardjones <simon.ward-jones@beamly.com>
2018-06-18 15:29:25 +0100

    pypi changes

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 
 setup(name='are_you_sure',
-      version='1.0',
+      version='1.0.0',
       description='simple decorator to check before running',
       author='Simon Ward-Jones',
       author_email='simonwardjones16@gmail.com',
       packages=['are_you_sure'],
+      license='MIT',
+      url='https://github.com/simonwardjones/are_you_sure',
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python']
       )
